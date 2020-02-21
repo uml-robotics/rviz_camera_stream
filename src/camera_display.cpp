a5eaf6f6c376b53229394175703fabcb6733579e
Zhao Han <cx.chenghai@gmail.com>
2020-02-20 20:16:43 -0500

    Update camera_display.cpp
    use the last transform

--- src/camera_display.cpp
+++ src/camera_display.cpp
@@ -607,7 +607,7 @@ bool CameraPub::updateCamera()
   Ogre::Vector3 position;
   Ogre::Quaternion orientation;
   const bool success = context_->getFrameManager()->getTransform(
-      info->header.frame_id, info->header.stamp, position, orientation);
+      info->header.frame_id, ros::Time(0), position, orientation);
   if (!success)
   {
     std::string error;
